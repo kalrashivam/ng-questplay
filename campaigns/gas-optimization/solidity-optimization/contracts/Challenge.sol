89061901e54d017e3272cf185e58869a9d9601de
kalrashivam <shivamkalra2017@gmail.com>
2023-10-25 16:55:50 +0530

    implement sumAllExceptSkip

--- campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.19;
 
 contract Challenge {
 
-    uint256 _SKIP;
+    uint256 immutable _SKIP;
 
     constructor(uint256 skip) {
         _SKIP = skip;
     }
 
-    /** 
+    /**
      * @notice Returns the sum of the elements of the given array, skipping any SKIP value.
      * @param array The array to sum.
      * @return sum The sum of all the elements of the array excluding SKIP.
@@ -17,9 +17,19 @@ contract Challenge {
     function sumAllExceptSkip(
         uint256[] calldata array
     ) public view returns (uint256 sum) {
+        uint256 arrayLength = array.length;
+        for (uint256 i; i < arrayLength;) {
+            uint256 cacheElement = array[i];
+            if (cacheElement != _SKIP) {
+                sum += cacheElement;
+            }
 
-        // IMPLEMENT HERE
-        
+            unchecked {
+                ++i;
+            }
+        }
+
+        return sum;
     }
 
 }
